9ed69360cc2baa7945422c0a51a519c440939105
Tcacia <137983436+TcaciaSR34@users.noreply.github.com>
2023-07-23 14:24:03 +0800

    Add files via upload

---
+++ c++_7_20.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+#define day  7
+
+int main()
+{
+	cout << "一个周有"<< day << "天" << endl;
+	const int month = 12;
+	cout << "一年有" << month << "个月" << endl;
+
+
+	system("pause");
+
+	return 0;
+
+
+}
